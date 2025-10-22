91bb30f79ef77bd851b112354f4a06ad426776dd
seongho5356 <kjeng7897@gmail.com>
2025-10-23 02:06:38 +0900

    [SRLT-7] Refactor: 파일명을 변경한다

--- src/main/java/starlight/application/businessplan/BusinessPlanServiceImpl.java
+++ src/main/java/starlight/application/businessplan/BusinessPlanServiceImpl.java
@@ -1,7 +1,6 @@
 package starlight.application.businessplan;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.descriptor.web.XmlEncodingBase;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import starlight.application.businessplan.provided.BusinessPlanService;
@@ -15,14 +14,12 @@ public class BusinessPlanServiceImpl implements BusinessPlanService {
 
     private final BusinessPlanQuery businessPlanQuery;
 
-    @Transactional
     public Long createBusinessPlan(Long memberId) {
         BusinessPlan plan = BusinessPlan.create(memberId);
 
         return businessPlanQuery.save(plan).getId();
     }
 
-    @Transactional
     public void deleteBusinessPlan(Long planId, Long memberId) {
         BusinessPlan plan = businessPlanQuery.getOrThrow(planId);
         if (!plan.isOwnedBy(memberId)) {
