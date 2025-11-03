ca2c4a62cc53d3ec717ccdc1adc9851b80e61589
seongho5356 <kjeng7897@gmail.com>
2025-11-03 17:29:49 +0900

    [SRLT-33] Chore: 파일명을 수정한다

---
+++ src/main/java/starlight/bootstrap/MailConfig.java
@@ -0,0 +1,45 @@
+package starlight.bootstrap;
+
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+@EnableConfigurationProperties(MailProperties.class)
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender(MailProperties mailProperties) {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(mailProperties.getHost());
+        mailSender.setPort(mailProperties.getPort());
+        mailSender.setUsername(mailProperties.getUsername());
+        mailSender.setPassword(mailProperties.getPassword());
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+
+        // 타임아웃 설정 (밀리초)
+        props.put("mail.smtp.connectiontimeout", "10000");
+        props.put("mail.smtp.timeout", "10000");
+        props.put("mail.smtp.writetimeout", "10000");
+
+        // SSL 설정 (필요한 경우)
+        if (mailProperties.getPort() == 465) {
+            props.put("mail.smtp.ssl.enable", "true");
+        }
+
+        props.putAll(mailProperties.getProperties());
+
+        return mailSender;
+    }
+}
