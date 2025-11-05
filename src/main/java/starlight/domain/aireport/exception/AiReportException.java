c1daf71131467284ba16febf2231545d5b1177b7
2ghrms <aa020228@gmail.com>
2025-11-05 20:18:33 +0900

    [SRLT-11] Feat: AI리포트 도메인 로직 작성
    - 엔티티 작성
    - 에러타입과 커스텀 예외

---
+++ src/main/java/starlight/domain/aireport/exception/AiReportException.java
@@ -0,0 +1,11 @@
+package starlight.domain.aireport.exception;
+
+import starlight.shared.apiPayload.exception.ErrorType;
+import starlight.shared.apiPayload.exception.GlobalException;
+
+public class AiReportException extends GlobalException {
+
+    public AiReportException(ErrorType errorType) {
+        super(errorType);
+    }
+}
