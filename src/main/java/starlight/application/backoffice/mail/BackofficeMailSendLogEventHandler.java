8168f2bb191597769ffcfa3f37bb50468b348045
seongho5356 <kjeng7897@gmail.com>
2026-01-25 15:15:49 +0900

    [SRLT-133] Refactor: 메일 전송 에러가 전파되지 않도록 한다

--- src/main/java/starlight/application/backoffice/mail/BackofficeMailSendLogEventHandler.java
+++ src/main/java/starlight/application/backoffice/mail/BackofficeMailSendLogEventHandler.java
@@ -1,23 +1,27 @@
 package starlight.application.backoffice.mail;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.dao.DataAccessException;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 import starlight.application.backoffice.mail.event.BackofficeMailSendEvent;
 import starlight.application.backoffice.mail.required.BackofficeMailSendLogCommandPort;
 import starlight.application.backoffice.mail.util.EmailMaskingUtils;
-import starlight.domain.backoffice.exception.BackofficeErrorType;
-import starlight.domain.backoffice.exception.BackofficeException;
 import starlight.domain.backoffice.mail.BackofficeMailSendLog;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class BackofficeMailSendLogEventHandler {
 
     private final BackofficeMailSendLogCommandPort logCommandPort;
 
-    @EventListener
+    @Async("emailTaskExecutor")
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handle(BackofficeMailSendEvent event) {
         String recipients = EmailMaskingUtils.maskRecipients(event.to());
 
@@ -32,7 +36,7 @@ public class BackofficeMailSendLogEventHandler {
         try {
             logCommandPort.save(log);
         } catch (DataAccessException exception) {
-            throw new BackofficeException(BackofficeErrorType.MAIL_LOG_SAVE_FAILED);
+            log.warn("[MAIL] send log save failed. subject={}", event.subject(), exception);
         }
     }
 }
