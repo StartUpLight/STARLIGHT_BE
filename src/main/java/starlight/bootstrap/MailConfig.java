07a4bd78a2efa531ef59c91a3b240310133b5a05
Hogeun Lee <aa020228@gmail.com>
2026-05-20 16:57:50 +0900

    [SRLT-155] Refactor: 개발용 Emailpit 등의 추가를 고려한 EmailConfig의 포트 추가

--- src/main/java/starlight/bootstrap/MailConfig.java
+++ src/main/java/starlight/bootstrap/MailConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.util.StringUtils;
 
 import java.util.Properties;
 
@@ -20,18 +21,22 @@ public class MailConfig {
         sender.setPort(mailProperties.getPort());
         sender.setUsername(mailProperties.getUsername());
         sender.setPassword(mailProperties.getPassword());
-        sender.setJavaMailProperties(buildJavaMailProps(mailProperties)); // ⬅ 추출된 메서드 사용
+        sender.setJavaMailProperties(buildJavaMailProps(mailProperties));
         return sender;
     }
 
     private Properties buildJavaMailProps(MailProperties mailProperties) {
         Properties props = new Properties();
         props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
 
-        if (mailProperties.getPort() == 465) {
+        boolean smtpAuth = StringUtils.hasText(mailProperties.getUsername())
+                && StringUtils.hasText(mailProperties.getPassword());
+        props.put("mail.smtp.auth", String.valueOf(smtpAuth));
+
+        int port = mailProperties.getPort() != null ? mailProperties.getPort() : 587;
+        if (port == 465) {
             props.put("mail.smtp.ssl.enable", "true");
-        } else {
+        } else if (port == 587) {
             props.put("mail.smtp.starttls.enable", "true");
             props.put("mail.smtp.starttls.required", "true");
         }
